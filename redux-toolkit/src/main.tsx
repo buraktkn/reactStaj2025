0cce56cdae2ad1a2171dded15723b6535be18b8b
buraktkn <burak.tekin@std.medipol.edu.tr>
2025-08-12 23:41:53 +0300

    Denemeyi counter olarak değiştirdik yeni bir app kurulacak

--- redux-toolkit/src/main.tsx
+++ redux-toolkit/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "app/app";
+import App from "app/1.counter";
 import { Provider } from "react-redux";
 import { store } from "app/store";
 
