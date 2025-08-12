d073545e997487e601849d96e07b58e39d54a10d
buraktkn <burak.tekin@std.medipol.edu.tr>
2025-08-12 22:17:47 +0300

    Redux toolkit için proje açıldı

---
+++ redux-toolkit/src/app/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>
+      <span className='text-6xl'>21</span>
+      <div className='flex'>
+        <button className='bg-blue-500 px-4 py-2 rounded text-white'>Arttır</button>
+        <button className='bg-red-500 px-4 py-2 rounded text-white'>Azalt</button>
+      </div>
+    </div>
+  )
+}
+
+export default app
